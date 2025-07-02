3137a1680ead0e5226105095b004c721dadf4777
jhpark2 <t2502021@partner.coway.com>
2025-07-02 10:49:03 +0900

    Revert "인트로화면 / height가 작은 화면에서 등록 버튼 깨지는 현상 수정"
    This reverts commit 07ca3bcb0592a4a73ba30ba80109edb857ee3605.

--- lib/presentation/intro/intro_screen.dart
+++ lib/presentation/intro/intro_screen.dart
@@ -21,22 +21,27 @@ class IntroScreen extends StatelessWidget {
           ),
           Align(
             child: Column(
-              mainAxisSize: MainAxisSize.min,
               children: [
-                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                const SizedBox(
+                  height: 104,
+                ),
                 Image.asset(
                   "assets/image/intro_bell.png",
                   width: 79,
                   height: 79,
                 ),
-                const SizedBox(height: 14),
+                const SizedBox(
+                  height: 14,
+                ),
                 Text(
                   "D35H App Push",
                   style: TextStyles.mediumTextBold.copyWith(
                     color: ColorStyle.white,
                   ),
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+                const SizedBox(
+                  height: 222,
+                ),
                 SizedBox(
                   width: 341,
                   child: Text(
@@ -47,14 +52,17 @@ class IntroScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(
+                  height: 20,
+                ),
                 Text(
                   "계속 진행 하시려면 등록을 눌러주세요.",
-                  style: TextStyles.normalTextRegular.copyWith(
-                    color: Colors.white,
-                  ),
+                  style: TextStyles.normalTextRegular
+                      .copyWith(color: Colors.white),
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                const SizedBox(
+                  height: 64,
+                ),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 66),
                   child: MediumButton(
